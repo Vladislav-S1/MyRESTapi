766e8b9d971978d9c9af78fac0ef40a4c9326f12
sadchikovvladislav <sadchikovvladislav@gmail.com>
2021-01-21 01:20:01 +0200

    2
    all files

---
+++ config/core.php
@@ -0,0 +1,26 @@
+<?php
+if (isset ($_POST['status']) && $_POST['status'] == 'test'){
+	echo ( json_encode($arrayName = array( "status" => "true", "error" => "" ) ) );
+	die();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+	<script src="/script.js"></script>
+</head>
+<body>
+	222222222222
+</body>
+</html>
+<?php
+?>
+
+
+
+
+
+
